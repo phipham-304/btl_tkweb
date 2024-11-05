502e7d7aef4dbd1bdb8af5f6218f177a0ca711f0
vo Thai <148705061+Theiz15@users.noreply.github.com>
2024-11-05 10:45:33 +0700

    moi

--- Thuc_Don/code/test.js
+++ Thuc_Don/code/test.js
@@ -8,14 +8,6 @@ document.getElementById('prev').onclick = function(){
 }
 
 
-// document.getElementById('nt').onclick = function(){
-//     const widthItem = document.querySelector('.item').offsetWidth;
-//     document.getElementById('form').scrollLeft += widthItem;
-// }
-// document.getElementById('pr').onclick = function(){
-//     const widthItem = document.querySelector('.item').offsetWidth;
-//     document.getElementById('form').scrollLeft -= widthItem;
-// }
 
 document.getElementById('next2').onclick = function(){
     const widthItem = document.querySelector('.item').offsetWidth;
@@ -44,27 +36,6 @@ document.getElementById('prev4').onclick = function(){
     document.getElementsByClassName('formList4')[0].scrollLeft -= widthItem;
 }
 
-
-
-const productContainer = document.querySelector('.item');
-
-productContainer.addEventListener('mousedown', function(e) {
-  if (e.button === 0) { // Click trái chuột
-    const startX = e.clientX;
-    let isDragging = false;
-
-    productContainer.addEventListener('mousemove', function(e) {
-      if (isDragging) {
-        const deltaX = startX - e.clientX;
-        productContainer.scrollLeft += deltaX;
-        startX = e.clientX;
-      }
-    });
-
-    productContainer.addEventListener('mouseup', function() {
-      isDragging = false;
-    });
-
-    isDragging = true;
-  }
-});
+function foodorder(name, price) {
+  alert(`Bạn đã đặt món ${name} thành công!\nVới giá: ${price}`);
+}
